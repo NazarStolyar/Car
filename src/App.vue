f83fd6d5304b1713329731dbe3afd242e01c22f7
NazarStolyar <batinak94@gmail.com>
2019-06-02 21:52:13 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+            app
+            temporary
+            v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+                v-for="link of links"
+                :key="link.title"
+                :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.titel"></v-list-tile-title>
+          </v-list-tile-content>
+
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+    <v-toolbar dark color="primary" app>
+          <v-toolbar-side-icon
+                  @click="drawer = true"
+                  class="hidden-md-and-up"
+          >
+          </v-toolbar-side-icon>
+          <v-toolbar-title>Cars</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn
+                flat
+                v-for="link in links"
+                :key="link.titel"
+                :to="link.url"
+            >
+              <v-icon left>{{link.icon}}</v-icon>
+              {{link.titel}}
+            </v-btn>
+          </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+ data () {
+     return {
+         drawer: false,
+         links: [
+             {
+                 titel: 'Login',
+                 icon: 'lock',
+                 url: '/login'
+
+             },
+             {
+                 titel: 'Registration',
+                 icon: 'account_circle',
+                 url: '/registeration'
+             },
+             {
+                 titel: 'Order',
+                 icon: 'bookmark_border',
+                 url: '/orders'
+             },
+             {
+                 titel: 'Add car',
+                 icon: 'note_add',
+                 url: '/add_car'
+             },
+             {
+                 titel: 'My cars',
+                 icon: 'list',
+                 url: '/my_cars'
+             }
+         ]
+     }
+ }
+}
+</script>
